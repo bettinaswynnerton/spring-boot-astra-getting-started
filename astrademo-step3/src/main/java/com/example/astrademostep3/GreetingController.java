81df027df355848ee86de1dbcbea916ee91402a5
Bettina Swynnerton <bettina.swynnerton@datastax.com>
2020-10-09 13:34:29 +0100

    typos

--- astrademo-step3/src/main/java/com/example/astrademostep3/GreetingController.java
+++ astrademo-step3/src/main/java/com/example/astrademostep3/GreetingController.java
@@ -1,4 +1,4 @@
-package com.example.astrademostep2;
+package com.example.astrademostep3;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
